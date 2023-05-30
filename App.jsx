b2712daaaf362db4f704b971d3b9a72962493fb3
salehsheikh <79030299+salehsheikh@users.noreply.github.com>
2023-05-31 01:06:06 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useRef, useLayoutEffect } from "react";
+import { useForm } from "./useForm";
+import { Hello } from "./Hello";
+import { useMeasure } from "./useMeasure";
+
+const App = () => {
+  const [values, handleChange] = useForm({
+    email: "",
+    password: "",
+    firstName: ""
+  });
+  const inputRef = useRef();
+  const hello = useRef(() => console.log("hello"));
+
+  const [showHello, setShowHello] = useState(true);
+  const[rect,inputRef2]=useMeasure([]);
+  // useLayoutEffect(()=>{
+  //   console.log(inputRef.current.getBoundingClientRect())
+  // },[])
+ 
+  return (
+    <div>
+      <>
+        <button onClick={() => setShowHello(!showHello)}>toggle</button>
+        {showHello && <Hello />}
+        <input
+          ref={inputRef}
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <input
+        ref={inputRef2}
+          name="firstName"
+          placeholder="first name"
+          value={values.firstName}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+        <button
+          onClick={() => {
+            inputRef.current.focus();
+            hello.current();
+          }}
+        >
+          focus
+        </button>
+      </>
+    </div>
+  );
+};
+
+export default App;
